d620e9d51d76d65a973f4001d0913329073e68b9
Kyle Aron <aron.kyle@gmail.com>
2023-01-19 15:39:30 -0500

    Convert the SplashPage to typescript

--- src/views/SplashPage/SplashPage.tsx
+++ src/views/SplashPage/SplashPage.tsx
@@ -5,18 +5,18 @@ import Header from '../../components/Header/Header'
 import SplashLogin from '../../components/SplashLogin/SplashLogin'
 
 
-export default function SplashPage({setSteamId}) {
+export default function SplashPage({setSteamId} : {setSteamId: (id:string) => void}) {
 
     const theme = useTheme();
 
     return (
-        <Container maxWidth="" sx={{ bgcolor: theme.body }}>
+        <Container maxWidth={false} sx={{ bgcolor: theme.body }}>
 
             <Container maxWidth="lg" sx={{  }}>
 
                 <Stack spacing={2} sx={{ height: '100vh', width: 'fill' }}>
 
-                    <Header />
+                    <Header userId={''}/>
 
                     <Box sx={{ display: 'flex', height: '50vh', justifyContent: 'center', alignItems: 'center' }}>
                         <SplashLogin setSteamId={setSteamId} />
