3c752d1efd7b63685b64fafd736a87335650c0ef
Kyle Aron <aron.kyle@gmail.com>
2023-01-24 12:29:24 -0500

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/dotabrief/i);
-  expect(headerElement).toBeInTheDocument();
-});
