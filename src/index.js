af1d80b5affdd9667572a10488f9b7952f4c7fb4
Kyle Aron <aron.kyle@gmail.com>
2023-02-07 17:28:32 -0500

    Use the CookiesProvider in the app.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CookiesProvider } from "react-cookie";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <CookiesProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </CookiesProvider>
   </React.StrictMode>
 );
 
